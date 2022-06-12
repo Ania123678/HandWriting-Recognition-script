e1fa83e237521361cd91abc727d9ec16f68c784e
Ania123678 <anna.kus084@gmail.com>
2022-06-12 17:47:19 +0200

    .

--- HandWriting-Recognition.py
+++ HandWriting-Recognition.py
@@ -31,7 +31,7 @@ for x in range(1,4):
     img = cv2.imread(f'{x}.png')[:,:,0]
     img = np.invert(np.array([img]))
     prediction = model.predict(img)
-    print(f'to jest prawdopodobnie:', np.argmax(prediction))
+    print(f'it looks like:', np.argmax(prediction))
     plt.imshow(img[0], cmap=plt.cm.binary)
     plt.show()
 
